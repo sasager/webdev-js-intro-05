a78d5dbc4c6c3f20505c093d08496209b4fabe26
Sierra <sierra.ar.sager@gmail.com>
2025-01-19 18:01:51 -0500

    completed assignment

--- js/assignment.js
+++ js/assignment.js
@@ -10,6 +10,8 @@ const todos = [
 
 // Declare your two immutable variables
 
+let shiftValue;
+let popValue;
 
 // Declaring constant variables to store references to the elements
 // that will be updated by your code.
@@ -22,43 +24,51 @@ const answerSixEl = document.getElementById("answer-six");
 const submissionBtn = document.getElementById("submission-btn");
 
 function updateAnswerOne() {
-    
+    answerOneEl.innerText = todos.join();
 }
 
 function updateAnswerTwo() {
-
+    answerTwoEl.innerText = todos[2];
 }
 
 function removeLastValue() {
-
+    popValue = todos.pop();
 }
 
 function removeFirstValue() {
-
+    shiftValue = todos.shift();
 }
 
 function addShiftAndPopValues() {
-
+    todos.push(shiftValue, popValue);
 }
 
 function updateAnswerFour() {
-
+    answerFourEl.innerText = todos.join();
 }
 
 function reverseTodoList() {
-
+    todos.reverse();
 }
 
 function updateAnswerFive() {
-
+    answerFiveEl.innerText = todos.join();
 }
 
 function updateAnswerSix() {
-
+    answerSixEl.innerText = todos.join();
 }
 
 function render() {
-
+    updateAnswerOne();
+    updateAnswerTwo();
+    removeLastValue();
+    updateAnswerFour();
+    reverseTodoList();
+    updateAnswerFive();
+    removeFirstValue();
+    addShiftAndPopValues();
+    updateAnswerSix();
 }
 
 submissionBtn.addEventListener("click", function () {
