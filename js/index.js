a1a026c01a893a4abc766d7b40174279b53d83e1
Sierra <sierra.ar.sager@gmail.com>
2025-01-19 17:25:17 -0500

    completed experiment

--- js/index.js
+++ js/index.js
@@ -1,3 +1,34 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+
+/*
+
+const pizzas = ["cheeese", "pepperoni", "veggie", "meatLovers", "supreme"];
+let lattes = ["mocha", "caramel", "vanilla"];
+const numbers = [24601, 8675309, 42, 1995];
+
+console.log(pizzas); // (5) ['cheeese', 'pepperoni', 'veggie', 'meatLovers', 'supreme']
+console.log(lattes); // (3) ['mocha', 'caramel', 'vanilla']
+console.log(numbers); // (4) [24601, 8675309, 42, 1995]
+
+let random = [24601, "pepperoni", true]; // (3) [24601, 'pepperoni', true]
+
+console.log(typeof pizzas); // object
+
+lattes.shift(); // 'mocha'
+console.log(lattes); // (2) ['caramel', 'vanilla']
+
+lattes.push("pumpkin"); // 3
+console.log(lattes); // (3) ['caramel', 'vanilla', 'pumpkin']
+
+pizzas[1] = "sausage"; // 'sausage'
+console.log(pizzas); // (5) ['cheeese', 'sausage', 'veggie', 'meatLovers', 'supreme']
+
+lattes[0] = "pistachio"; // 'pistachio'
+console.log(lattes); // (3) ['pistachio', 'vanilla', 'pumpkin']
+
+numbers[2] = 2025; // '2025'
+console.log(numbers); // (4) [24601, 8675309, 2025, 1995]
+
+*/
